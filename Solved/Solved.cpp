365d4c0804cf50e7ab5a0d3b788a16796d9aecbe
eazuooz <aqaurius4785@gmail.com>
2024-01-03 00:38:34 +0900

    초기 커밋.

---
+++ Solved/Solved.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int Add()
+{
+    int a = 0;
+    int b = 0;
+
+    std::cin >> a >> b;
+
+    return a + b;
+}
+
+int main()
+{
+    int ret = Add();
+    std::cout << ret;
+
+    return 0;
+}
+
