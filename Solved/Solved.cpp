87f6792c4b5a638ee24a4394e99d071616f880a3
LeeJunJae <46310910+eazuooz@users.noreply.github.com>
2024-01-16 02:01:26 +0900

    2557

--- Solved/Solved.cpp
+++ Solved/Solved.cpp
@@ -3,37 +3,10 @@
 #include <algorithm>
 #include <string>
 
-char GetManyUseAlphabet(std::string& str)
-{
-    std::vector<int> bucket = {};
-    bucket.resize(256);
-
-    for (size_t i = 0; i < str.length(); i++)
-    {
-        int idx = static_cast<int>(str[i]);
-        if (str[i] >= 'a')
-        {
-            idx -= 32;
-        }
-
-        bucket[idx]++;
-    }
-
-    char alphabet = max_element(bucket.begin(), bucket.end()) - bucket.begin();
-    
-    int count = std::count(bucket.begin(), bucket.end(), bucket[alphabet]);
-    if (count >= 2)
-        alphabet = '?';
-
-    return alphabet;
-}
 
 int main()
 {
-    std::string str = "";
-    std::getline(std::cin, str);
-    
-    std::cout << GetManyUseAlphabet(str) << "\n";
+    std::cout << "Hello World!" << "\n";
 
     return 0;
 }
