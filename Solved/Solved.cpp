aabb46c43f3fea79eb4320e5b8efa7abda2f9f06
LeeJunJae <46310910+eazuooz@users.noreply.github.com>
2024-01-23 16:49:27 +0900

    2753

--- Solved/Solved.cpp
+++ Solved/Solved.cpp
@@ -9,13 +9,29 @@
 
 int main()
 {
-    int number = 0;
-    std::cin >> number;
+    int year = 0;
+    std::cin >> year;
 
-    for (size_t i = 0; i < number; i++)
-    {
-        std::cout << i + 1 << "\n";
-    }
+    bool leapYear = false;
+    bool multiplesOf4 = false;
+    bool multiplesOf100 = false;
+    bool multiplesOf400 = false;
+
+    year % 4 == 0 ? multiplesOf4 = true : multiplesOf4 = false;
+    year % 100 == 0 ? multiplesOf100 = true : multiplesOf100 = false;
+    year % 400 == 0 ? multiplesOf400 = true : multiplesOf400 = false;
+
+    if (multiplesOf4 && !multiplesOf100)
+        leapYear = true;
+
+    if (multiplesOf100 && multiplesOf400)
+        leapYear = true;
+    
+
+    if (leapYear)
+        std::cout << 1;
+    else
+        std::cout << 0;
 
     return 0;
 }
