b3c2539d1159396a18f6aae7006841e6a4700de6
LeeJunJae <46310910+eazuooz@users.noreply.github.com>
2024-02-26 19:19:09 +0900

    3052

--- Solved/Solved.cpp
+++ Solved/Solved.cpp
@@ -7,15 +7,56 @@
 #include <unordered_map>
 
 
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <unordered_map>
+
+
+//int arr[42];
+//
+//int main() {
+//    int x;
+//    int ans = 0;
+//
+//    for (int i = 0; i < 10; i++) {
+//        scanf("%d", &x);
+//        arr[x % 42] = 1;
+//    }
+//
+//    for (int i = 0; i < 42; i++)
+//        ans += arr[i];
+//
+//    printf("%d", ans);
+//
+//    return 0;
+//}
+
 int main()
 {
-    int number = 0;
-    std::cin >> number;
-
-    for (size_t i = 0; i < number; i++)
+    std::vector<int> inputs;
+    inputs.resize(42);
+    
+    for (size_t i = 0; i < 10; i++)
     {
-        std::cout << i + 1 << "\n";
+        int num = 0;
+        std::cin >> num;
+        inputs[num % 42]++;
     }
+
+    int remain = 0;
+    for (int num : inputs)
+    {
+        if (num == 0)
+            continue;
+
+        remain++;
+    }
+    
+    std::cout << remain;
 
     return 0;
 }
