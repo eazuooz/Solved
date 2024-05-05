6c82d0950dcd590204ceea7ff26f816d7b988bed
LEEJUNJAE <46310910+eazuooz@users.noreply.github.com>
2024-05-05 23:13:32 +0900

    9498

--- Solved/Solved.cpp
+++ Solved/Solved.cpp
@@ -1,12 +1,4 @@
-﻿#include <iostream>
-#include <vector>
-#include <cmath>
-#include <algorithm>
-#include <string>
-#include <sstream>
-#include <unordered_map>
-
-
+﻿#pragma region LIB
 #include <iostream>
 #include <vector>
 #include <cmath>
@@ -14,49 +6,36 @@
 #include <string>
 #include <sstream>
 #include <unordered_map>
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <unordered_map>
+#pragma endregion
 
-
-//int arr[42];
-//
-//int main() {
-//    int x;
-//    int ans = 0;
-//
-//    for (int i = 0; i < 10; i++) {
-//        scanf("%d", &x);
-//        arr[x % 42] = 1;
-//    }
-//
-//    for (int i = 0; i < 42; i++)
-//        ans += arr[i];
-//
-//    printf("%d", ans);
-//
-//    return 0;
-//}
+char CalculateScore(const int score)
+{
+    if (score >= 60 && score <= 69)
+		return 'D';
+	if (score >= 70 && score <= 79)
+		return 'C';
+	if (score >= 80 && score <= 89)
+		return 'B';
+	if (score >= 90 && score <= 100)
+		return 'A';
+		
+	return 'F';
+}
 
 int main()
 {
-    std::vector<int> inputs;
-    inputs.resize(42);
-    
-    for (size_t i = 0; i < 10; i++)
-    {
-        int num = 0;
-        std::cin >> num;
-        inputs[num % 42]++;
-    }
+    int score = 0;
+    std::cin >> score;
 
-    int remain = 0;
-    for (int num : inputs)
-    {
-        if (num == 0)
-            continue;
-
-        remain++;
-    }
-    
-    std::cout << remain;
+    char grade = CalculateScore(score);
+	std::cout << grade;
 
     return 0;
 }
