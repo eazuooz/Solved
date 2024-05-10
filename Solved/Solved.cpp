c240e01679fbac28a18196b8a9cfc7472b61f569
LEEJUNJAE <46310910+eazuooz@users.noreply.github.com>
2024-05-10 21:55:50 +0900

    18110

--- Solved/Solved.cpp
+++ Solved/Solved.cpp
@@ -5,35 +5,43 @@
 #include <algorithm>
 #include <string>
 #include <vector>
+#include <queue>
 #include <unordered_map>
 #include <stack>
+#include <numeric>
 #pragma endregion
-
-std::pair<int, int> fibonacci(int n)
-{
-	std::pair<int, int> dp[41] = { {1,0}, {0,1}, };
-	for (size_t i = 2; i <= n; i++)
-	{
-		dp[i].first = dp[i - 1].second;
-		dp[i].second = dp[i - 1].first + dp[i - 1].second;
-	}
-
-	return std::make_pair(dp[n].first, dp[n].second);
-}
 
 int main()
 {
-	int testCase = 0;
-	std::cin >> testCase;
-	for (size_t i = 0; i < testCase; i++)
+	int n = 0;
+	std::cin >> n;
+	float fifteenPercent = (float)n * 0.15f;
+	int cuttingCount = round(fifteenPercent);
+
+	if (n == 0)
+	{
+		std::cout << 0;
+		return 0;
+	}
+
+	std::vector<int> vector = {};
+	for (size_t i = 0; i < n; i++)
 	{
 		int num = 0;
 		std::cin >> num;
-		std::pair<int, int> ret = fibonacci(num);
-
-		std::cout << ret.first << " " << ret.second << "\n";
+		vector.push_back(num);
 	}
+
+	std::sort(vector.begin(), vector.end());
+	
+	float sum = 0.0f;
+	for (size_t i = cuttingCount; i < n - cuttingCount; i++)
+	{
+		sum += vector[i];
+	}
+	
+	float average = round(sum / (n - cuttingCount * 2));
+	std::cout << (int)average;
 
 	return 0;
 }
-
