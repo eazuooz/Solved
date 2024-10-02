dd900f60330d8b7e555aef794ce59a365311805f
LEEJUNJAE <eazuooz@gmail.com>
2024-10-02 10:32:01 +0900

    Find the Index of the First Occurrence in a String

--- Solved/Solved.cpp
+++ Solved/Solved.cpp
@@ -16,66 +16,21 @@
 
 using namespace std;
 
-// String to Integer (atoi)
-int myAtoi(string s)
+//Find the Index of the First Occurrence in a String
+int strStr(string haystack, string needle)
 {
-	long long int ans = 0;
-	int i = 0;
+	int ret = -1;
+	ret = haystack.find(needle);
 
-	// Skip leading whitespaces
-	while (s[i] == ' ') 
-		i++;
-
-	int sign = 0;   // 0 -> + , 1 -> -
-
-	// Handle signs
-	if (s[i] == '-') 
-	{
-		sign = 1;
-		i++;
-	}
-	else if (s[i] == '+') 
-	{
-		sign = 0;
-		i++;
-	}
-
-	// Skip leading zeros
-	while (s[i] == '0') 
-		i++;
-
-	// Convert characters to integer
-	while (s[i] >= '0' && s[i] <= '9') 
-	{
-		if (ans > INT_MAX) 
-		{
-			if (sign) 
-				return INT_MIN;
-
-			return INT_MAX;
-		}
-
-		ans = ans * 10 + s[i] - '0';
-		i++;
-	}
-
-	// Apply sign
-	if (sign) 
-		ans *= -1;
-
-	// Handle overflow/underflow
-	if (ans > INT_MAX) return INT_MAX;
-	if (ans < INT_MIN) return INT_MIN;
-
-	return (int)ans;
+	return haystack.find(needle);
 }
 
 
 int main()
 {		  
-	myAtoi("+-12");
+	strStr("sadbutsad", "zzz");
 		  
-	int a = atoi("0-1");
+
 
 	return 0;
 }
