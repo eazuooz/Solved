03df6feaf4cdda7c41b5c63e61c918daa66ecdc2
LEEJUNJAE <46310910+eazuooz@users.noreply.github.com>
2025-03-19 19:15:09 +0900

    Best Time to Buy and Sell Stock
    https://leetcode.com/explore/featured/card/top-interview-questions-easy/97/dynamic-programming/572/

--- Solved/Solved.cpp
+++ Solved/Solved.cpp
@@ -18,49 +18,48 @@
 
 using namespace std;
 
-//🔸 점화식 적용 (Bottom-Up)
-//cpp
-//복사
-//편집
-//for(int i = 1; i <= n; i++) {
-//    dp[i] = dp[i-1];  // 한 칸 올라온 경우
-//    if(i > 1)
-//        dp[i] += dp[i-2];  // 두 칸 올라온 경우
-//}
-//dp[i] = dp[i-1]: (i-1)번째 계단에서 한 칸 올라왔을 때
-//dp[i] += dp[i-2]: (i-2)번째 계단에서 두 칸 올라왔을 때
-//즉, 점화식:
-//
-//dp[i] = dp[i-1] + dp[i-2]
-//
-//🔹 예제 실행 (n=5)
-//i	dp[i-2]	dp[i-1]	dp[i] = dp[i-1] + dp[i-2]
-//1	X	1	1
-//2	1	1	2
-//3	1	2	3
-//4	2	3	5
-//5	3	5	8
+//lsf (Least So Far): 지금까지 나온 가장 작은 주가를 저장하는 변수입니다.
+//op (Overall Profit): 지금까지 가능한 최대 이익을 저장하는 변수입니다.
+//pist (Profit If Sold Today): 오늘 주식을 판다면 얻을 수 있는 이익을 계산하는 변수입니다.
 
-int climbStairs(int n)
+//i	prices[i]	lsf (최소 주가)	pist (이익)	op (최대 이익)
+//0	7	7	0	0
+//1	1	1	0	0
+//2	5	1	4	4
+//3	3	1	2	4
+//4	6	1	5	5
+//5	4	1	3	5
+//💡 최대 이익은 5 (1에 사서 6에 팔기)입니다.
+
+int maxProfit(vector<int>& prices)
 {
-	vector<int> dp(n + 1, 0);
-	dp[0] = 1;
+	int lsf = INT_MAX;
+	int op = 0;
+	int pist = 0;
 
-	for (int i = 1; i <= n; i++)
+	for (int i = 0; i < prices.size(); i++)
 	{
-		dp[i] = dp[i - 1];
+		if (prices[i] < lsf)
+		{
+			lsf = prices[i];
+		}
 
-		if (i > 1)
-			dp[i] += dp[i - 2];
+		pist = prices[i] - lsf;
+
+		if (op < pist)
+		{
+			op = pist;
+		}
 	}
 
-	return dp[n];
+	return op;
 }
 
 
 int main()
 {
-	climbStairs(5);
+	vector<int> prices = { 7, 1, 5, 3, 6, 4 };
+	cout << maxProfit(prices) << endl;
 
 	return 0;
 }
